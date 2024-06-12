2dbe868d2a826e8aee954047f8696390f2a9abce
george.botos.no1@gmail.com <george.botos.no1@gmail.com>
2024-06-12 21:05:31 +0100

    stream endpoint in reactive controller

--- src/main/java/com/botos/webfluxplayground/sec01/ReactiveWebController.java
+++ src/main/java/com/botos/webfluxplayground/sec01/ReactiveWebController.java
@@ -1,6 +1,7 @@
 package com.botos.webfluxplayground.sec01;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,16 @@ public class ReactiveWebController {
 
 	@GetMapping("products")
 	public Flux<Product> getProducts() {
+		return webClient.get()
+		                .uri("/demo01/products")
+		                .retrieve()
+		                .bodyToFlux(Product.class)
+		                .doOnNext(product -> log.info("received: {}", product));
+	}
+
+	@GetMapping(value = "products/stream",
+	            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Product> getProductsStream() {
 		return webClient.get()
 		                .uri("/demo01/products")
 		                .retrieve()
