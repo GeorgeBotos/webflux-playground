a664b4d32e1ce8bd1e1a018df43e9e6a3dc15485
george.botos.no1@gmail.com <george.botos.no1@gmail.com>
2024-07-08 08:37:31 +0100

    sec07 -> request with headers

--- src/test/java/com/botos/webfluxplayground/sec07/Lec04HeaderTest.java
+++ src/test/java/com/botos/webfluxplayground/sec07/Lec04HeaderTest.java
@@ -4,26 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-class Lec03PostTest {
+import java.util.Map;
+
+class Lec04HeaderTest {
 
 	private static final String BASE_URL = "http://localhost:7070/demo02";
 
 	private static final Logger log = LoggerFactory.getLogger(Lec01MonoClientTest.class);
 
 	@Test
-	void testPostRequest() {
+	void testDefaultHeader() {
 		WebClient.builder()
 		         .baseUrl(BASE_URL)
+		         .defaultHeader("caller-id", "order-service")
 		         .build()
-		         .post()
-		         .uri("/lec03/product")
-		         .bodyValue(Product.builder()
-		                           .description("iphone")
-		                           .price(1000)
-		                           .build())
+		         .get()
+		         .uri("/lec04/product/{id}", 1)
 		         .retrieve()
 		         .bodyToMono(Product.class)
 		         .doOnNext(product -> log.info(product.toString()))
@@ -33,17 +31,15 @@ class Lec03PostTest {
 	}
 
 	@Test
-	void testPostWithMonoRequest() {
-		var productMono = Mono.fromSupplier(() -> Product.builder()
-		                                                 .description("iphone")
-		                                                 .price(1000)
-		                                                 .build());
+	void testHeadersWithMap() {
+		var headerValues = Map.ofEntries(Map.entry("caller-id", "order-service"),
+		                                 Map.entry("some-key", "some-value"));
 		WebClient.builder()
 		         .baseUrl(BASE_URL)
 		         .build()
-		         .post()
-		         .uri("/lec03/product")
-		         .body(productMono, Product.class)
+		         .get()
+		         .uri("/lec04/product/{id}", 1)
+		         .headers(headers -> headers.setAll(headerValues))
 		         .retrieve()
 		         .bodyToMono(Product.class)
 		         .doOnNext(product -> log.info(product.toString()))
